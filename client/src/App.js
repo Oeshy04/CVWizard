a04cd972075f89b448c58e1e1141f7980adfdaf6
Suraiya Hasan <83490460+Suraiya-Hasan@users.noreply.github.com>
2023-03-18 14:09:53 +0600

    initializing home,login and register pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import "antd/dist/antd.css"
+import "antd/dist/antd.min.css"
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import { Button, Space } from 'antd';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <h1>CVWizard</h1>
+      {/* <h1>CVWizard</h1>
       <h5>Create professional CV</h5>
-      <Button type='primary'>Create CV</Button>
+      <Button type='primary'>Create CV</Button> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
